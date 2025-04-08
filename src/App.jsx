4edaf57d3c0c7ffcf36d014d04275a37b449b9b7
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-08 12:19:02 -0400

    finished "Router Configuration" tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import { Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
@@ -10,7 +10,12 @@ function App() {
       <div id="container">
         <h1>Hello React Router!</h1>
         <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<h1>Blue</h1>}/>
+            <Route path="/red" element={<h1>Red</h1>}/>
+          </Routes>
+        </div>
       </div>
     </>
   )
